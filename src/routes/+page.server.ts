d20a398e6ce869d391e984a2de07763751da6f2d
shamokit <contact@shamokit.com>
2024-01-11 23:16:37 +0900

    test

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,17 @@
 export const prerender = true;
-export async function load({ fetch }) {
+export const load = async ({ fetch }) => {
+	const [qiitaResponse, zennResponse] = await Promise.all([
+		fetch(
+			'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fqiita.com%2Fshamokit%2Ffeed.atom'
+		),
+		fetch(
+			'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fzenn.dev%2Fshamokit%2Ffeed'
+		),
+	]);
+	const [qiita, zenn] = await Promise.all([
+		qiitaResponse.json(),
+		zennResponse.json(),
+	]);
 	const [response1, response2] = await Promise.all([
 		fetch('https://svatus.pages.dev/status/200'),
 		fetch('https://svatus.pages.dev/status/200'),
@@ -9,7 +21,9 @@ export async function load({ fetch }) {
 		response2.json(),
 	]);
 	return {
+		zenn: zenn.items.slice(0, 3),
+		qiita: qiita.items.slice(0, 3),
 		data1,
 		data2,
 	};
-}
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	let { data } = $props();
-	let { data1, data2 } = data;
+	let { data1, data2, zenn, qiita } = data;
 </script>
 
 test data1: {JSON.stringify(data1)}<br />
 data2: {JSON.stringify(data2)}
+
+zenn: {JSON.stringify(zenn)}<br />
+qiita: {JSON.stringify(qiita)}<br />
