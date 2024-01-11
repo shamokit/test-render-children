8a2bc1a4bda89b487cfee1a9a0b3ddeee5a2145a
shamokit <contact@shamokit.com>
2024-01-11 23:23:55 +0900

    test

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,3 +1,7 @@
+import {
+	INSTAGRAM_BUSINESS_ACCOUNT_ID,
+	INSTAGRAM_ACCESS_TOKEN,
+} from '$env/static/private';
 import { client } from '$lib/microcms.server';
 
 export const prerender = true;
@@ -28,10 +32,28 @@ export const load = async ({ fetch }) => {
 			},
 		}),
 	]);
+
+	const instagramUrl = `https://graph.facebook.com/v15.0/${INSTAGRAM_BUSINESS_ACCOUNT_ID}?fields=name,media.limit(12){media_url,permalink,caption}&access_token=${INSTAGRAM_ACCESS_TOKEN}`;
+	const photosResponse = await fetch(instagramUrl);
+	const photos = (await photosResponse.json()) as {
+		media: {
+			data: {
+				id: string;
+				media_url: string;
+				permalink: string;
+				caption: string;
+			}[];
+		};
+	};
 	return {
 		zenn: zenn.items.slice(0, 3),
 		qiita: qiita.items.slice(0, 3),
 		thinkings,
+		photos: photos.media.data.map(({ permalink, media_url, caption }) => ({
+			permalink,
+			media_url,
+			caption,
+		})),
 		data1,
 		data2,
 	};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	let { data } = $props();
-	let { data1, data2, zenn, qiita, thinkings } = data;
+	let { data1, data2, zenn, qiita, thinkings, photos } = data;
 </script>
 
 test data1: {JSON.stringify(data1)}<br />
@@ -8,4 +8,5 @@ data2: {JSON.stringify(data2)}
 
 zenn: {JSON.stringify(zenn)}<br />
 qiita: {JSON.stringify(qiita)}<br />
-thinkings: {JSON.stringify(thinkings)}
+thinkings: {JSON.stringify(thinkings)}<br />
+photos: {JSON.stringify(photos)}
