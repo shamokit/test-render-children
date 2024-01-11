fad52e24c56bd27a2494340719b1ac394fb1688f
shamokit <contact@shamokit.com>
2024-01-11 23:04:50 +0900

    test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+	import { page } from '$app/stores';
   import Test from '../lib/Test.svelte'
   let { children } = $props();
 </script>
 
 <div class="test">
   <Test />
+  {$page.route.id}
   <div class="test2">
     {@render children()}
   </div>
