1f697a42f7cdb884738a75ea6cd02b0e633c8403
shamokit <contact@shamokit.com>
2024-01-11 23:34:18 +0900

    test

---
+++ src/lib/Image.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import IntersectionObserver from 'svelte-intersection-observer';
+
+	let elementOnce: HTMLElement;
+	let intersectOnce: boolean;
+	const src = 'https://dummyimage.com/600x400/000/fff';
+	$: isIntersecting = false;
+</script>
+
+<IntersectionObserver
+	once
+	element="{elementOnce}"
+	on:observe="{(e) => {
+		isIntersecting = e.detail.isIntersecting;
+	}}"
+	bind:intersecting="{intersectOnce}"
+>
+	<img
+		bind:this="{elementOnce}"
+		src="{isIntersecting
+			? src
+			: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAQSURBVHgBAQUA+v8ADVBp/wL6AcYxvWp0AAAAAElFTkSuQmCC'}"
+		alt=""
+		width="600"
+		height="400"
+	/>
+</IntersectionObserver>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Image from '../lib/Image.svelte';
 	let { data } = $props();
 	let { data1, data2, zenn, qiita, thinkings, photos } = data;
 </script>
@@ -13,3 +14,5 @@ zenn: {JSON.stringify(zenn)}<br />
 qiita: {JSON.stringify(qiita)}<br />
 thinkings: {JSON.stringify(thinkings)}<br />
 photos: {JSON.stringify(photos)}
+
+<Image />
