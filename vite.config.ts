c16efd3bcb3ffc921511fa552e0d898ba7633848
shamokit <contact@shamokit.com>
2024-01-12 00:22:33 +0900

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-
-export default defineConfig({
+import type { UserConfig } from 'vite';
+const config: UserConfig = {
 	plugins: [sveltekit()]
-});
+};
+export default config;
